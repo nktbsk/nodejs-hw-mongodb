35591ce1b2c4d62977353ce9d1a42786a485c2f0
nktbsk <nikita.bashkir01@gmail.com>
2025-01-12 12:29:43 +0200

    fix post contacts add photo

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -46,9 +46,22 @@ export const getContactByIdController = async (req, res, next) => {
 };
 
 export const createContactController = async (req, res) => {
+  const photo = req.file;
+
+  let photoUrl;
+
+  if (photo) {
+    if (env('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
+
   const payload = {
     ...req.body,
     userId: req.user._id,
+    photo: photoUrl,
   };
   const contact = await services.createContact(payload);
 
