ffbaf7e33997ccfa2ffb8b2f44fbeefcd8d4606f
nktbsk <nikita.bashkir01@gmail.com>
2025-01-15 11:29:14 +0200

    HomeWork

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import router from './routers/index.js';
 import { env } from './utils/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 import { UPLOAD_DIR } from './constants/index.js';
 
 const PORT = Number(env('PORT', '3000'));
@@ -32,6 +33,9 @@ export const setupServer = () => {
     });
   });
 
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+
   app.use(router);
 
   app.use('*', notFoundHandler);
@@ -41,6 +45,4 @@ export const setupServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-
-  app.use('/uploads', express.static(UPLOAD_DIR));
 };
